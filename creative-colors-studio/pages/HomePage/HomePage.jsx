245d7dc7b38870600112ab055d570e1234718303
raghavPu <raghavsrisurya@gmail.com>
2021-07-23 10:33:45 -0500

    Finish Home Screen
    Finish Home Screen Design Component

---
+++ creative-colors-studio/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationBar from '../../src/components/NavigationBar'
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+        </div>
+    )
+}
+
+export default HomePage
---
+++ creative-colors-studio/pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import HomePage from './HomePage/HomePage'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <HomePage></HomePage>
+    </div>
+  )
+}
